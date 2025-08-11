106309200ab03685e90f1e459547bf553f19f1d7
suongMai <suong05mt@gmail.com>
2025-08-11 23:29:32 +0300

    get port
    print out port to test

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def main():
     application.add_handler(main_menu_handler)
     application.add_handler(CommandHandler("report", report_handler))
     application.add_handler(CommandHandler("remind_unposted", remind_unposted_handler))
-
+    print(int(os.getenv("PORT", "8000")))
     application.run_webhook(
         listen="0.0.0.0",
         port=int(os.getenv("PORT", "8000")),
